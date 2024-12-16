218392fcb747e8b88220acd44ec84c498cdfc4b0
nasirdn <124560829+nasirdn@users.noreply.github.com>
2024-12-16 18:01:40 +0300

    лр3

--- sem5/lr3/README.md
+++
@@ -1,9 +0,0 @@
-# Weather App
-
-Приложение для получения данных о погоде с OpenWeather API
-
-# Установка
-
-```
-pip install weather_app
-```
--- sem5/lr3/weather_app/main.py
+++
@@ -1,9 +0,0 @@
-from .own_key import key
-from .getweatherdata import get_weather_data
-
-def main():
-    city = input('Введите город на английском: ')
-    get_weather_data(city, key)
-
-if __name__ == '__main__':
-    main()
